e3828933a3dd71d084fb5ea8b483422c427e33c1
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2020-11-30 21:03:34 +0300

    Create train_test_split.py

---
+++ datasets/train_test_split.py
@@ -0,0 +1,14 @@
+import os
+import random
+def random_split(data_path, n_train):
+  train_paths = []
+  test_paths = []
+  for class_path in os.listdir(data_path):
+    cur_path = os.path.join(data_path, class_path)
+    img_paths = os.listdir(cur_path)
+    ids = range(len(img_paths))
+    train_ids = random.sample(ids, n_train)
+    test_ids = [i for i in ids if i not in train_ids]
+    train_paths.extend([os.path.join(cur_path, img_paths[i]) for i in train_ids])
+    test_paths.extend([os.path.join(cur_path, img_paths[i]) for i in test_ids])
+  return train_paths, test_paths
